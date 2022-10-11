33d7e0faf6e7490c8eff7d899dd7e5274fd4c1a0
Bas Smit <bas.smit@live.nl>
2022-10-11 10:47:46 +0200

    Update script.py

--- Infra/Startsemester/Nmap-python/Version_3/script.py
+++ Infra/Startsemester/Nmap-python/Version_3/script.py
@@ -4,16 +4,19 @@ import json
 nmap = nmap3.Nmap()
 nmap_scan = nmap3.NmapScanTechniques()
 
-#IP_scan = input("Enter IP adress to scan: ")
-IP_scan = '127.0.0.1'
+IP_scan = input("Enter IP adress to scan: ")
+#IP_scan = '127.0.0.1'
+Port = input("Enter number of top ports to scan (max=10): ")
+Port_scan = int(Port)
 
 results = nmap.scan_top_ports(IP_scan)
 
 # Printing JSON
 print("--------------")
-for i in range(5):
-   json_object = json.dumps(results['127.0.0.1']['ports'][i], indent = 4)
+for i in range(Port_scan):
+   json_object = json.dumps(results[IP_scan]['ports'][i], indent = 4)
    json_load = json.loads(json_object)
-   print("portid: ", json_load['portid'])
-   print("state: ", json_load['state'])
-   print("--------------")
+   if (results[IP_scan]['ports'][i]['state'] == 'open'):
+      print("portid: ", json_load['portid'])
+      print("state: ", json_load['state'])
+      print("--------------")
